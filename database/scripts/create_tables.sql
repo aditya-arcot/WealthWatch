c529bfca81b44bf69b65a6a5ff2e9ccb0a359e85
aditya-arcot <arcot.aditya@utexas.edu>
2024-06-20 01:09:14 -0500

    strict tables

--- database/scripts/create_tables.sql
+++ database/scripts/create_tables.sql
@@ -6,19 +6,19 @@ CREATE TABLE IF NOT EXISTS users (
     last_name TEXT NOT NULL,
     salt TEXT,
     hash TEXT
-);
+) STRICT;
 
 CREATE TABLE IF NOT EXISTS accounts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
-);
+) STRICT;
 
 CREATE TABLE IF NOT EXISTS categories (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
-);
+) STRICT;
 
 CREATE TABLE IF NOT EXISTS transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     category_id INTEGER NOT NULL,
     amount REAL NOT NULL,
     description TEXT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    timestamp TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (account_id) REFERENCES accounts(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
-);
+) STRICT;
