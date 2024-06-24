6c59a08ac62926f502e432e42f845ad553ea3591
aditya-arcot <arcot.aditya@utexas.edu>
2024-06-24 01:37:36 -0500

    express error

--- server/src/controllers/secretsController.ts
+++ server/src/controllers/secretsController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
 import { env } from 'process'
 import { ClientSecrets } from '../models/clientSecrets.js'
+import { ExpressError } from '../models/expressError.js'
 import { logger } from '../utils/logger.js'
 
 export const getSecrets = async (_req: Request, res: Response) => {
     logger.info('getting secrets')
     if (!env['CLIENT_LOGTAIL_TOKEN']) {
-        return res.status(500).send('missing one or more secrets')
+        throw new ExpressError('missing one or more secrets', 500)
     }
     const secrets: ClientSecrets = {
         logtailToken: env['CLIENT_LOGTAIL_TOKEN'],
