6087d527c63a922283dc5e9448409c27bf82215d
aditya-arcot <arcot.aditya@utexas.edu>
2024-07-03 01:42:26 -0500

    display current user's name

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
+import { UserService } from '../../services/user.service'
 
 @Component({
     selector: 'app-header',
     standalone: true,
     imports: [RouterLink, RouterLinkActive],
     templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+    public userName: string | undefined
+
+    constructor(private userSvc: UserService) {}
+
+    ngOnInit(): void {
+        const user = this.userSvc.getCurrentUser()
+        this.userName = user?.firstName
+    }
+}
