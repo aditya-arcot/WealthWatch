05c2f06661d19372aab98a67bd228cd39f466cd6
aditya-arcot <arcot.aditya@utexas.edu>
2024-07-15 10:52:04 -0500

    remove incomplete pages from prod

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router'
-import { AccountsComponent } from './pages/accounts/accounts.component'
 import { HomeComponent } from './pages/home/home.component'
 import { LoginComponent } from './pages/login/login.component'
 import { LogoutComponent } from './pages/logout/logout.component'
 import { RegisterComponent } from './pages/register/register.component'
 import { StartupErrorComponent } from './pages/startup-error/startup-error.component'
-// import { TransactionsComponent } from './pages/transactions/transactions.component'
 
 export const routes: Routes = [
     { path: 'startup-error', component: StartupErrorComponent },
@@ -13,8 +11,6 @@ export const routes: Routes = [
     { path: 'logout', component: LogoutComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'home', component: HomeComponent },
-    { path: 'accounts', component: AccountsComponent },
-    // { path: 'transactions', component: TransactionsComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ]
