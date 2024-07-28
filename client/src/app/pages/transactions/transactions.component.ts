95fca5c74e93d89f452ac5bbe415ab9f1ffcd7ec
aditya-arcot <arcot.aditya@utexas.edu>
2024-07-28 03:14:50 -0500

    improve transactions data formatting

--- client/src/app/pages/transactions/transactions.component.ts
+++ client/src/app/pages/transactions/transactions.component.ts
@@ -12,6 +12,12 @@ import { TransactionService } from '../../services/transaction.service'
 })
 export class TransactionsComponent implements OnInit {
     transactions: Transaction[] = []
+    currencyFormatters: Record<string, Intl.NumberFormat> = {
+        USD: new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        }),
+    }
 
     constructor(
         private transactionSvc: TransactionService,
@@ -27,5 +33,28 @@ export class TransactionsComponent implements OnInit {
             this.logger.debug('loaded transactions', t)
             this.transactions = t
         })
+    }
+
+    formatCurrency(t: Transaction): string {
+        const currency = t.unofficialCurrencyCode ?? t.isoCurrencyCode
+        if (!currency) return t.amount.toString()
+
+        if (!this.currencyFormatters[currency]) {
+            this.currencyFormatters[currency] = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency,
+            })
+        }
+
+        return this.currencyFormatters[currency].format(t.amount)
+    }
+
+    formatCategory(t: Transaction): string {
+        if (!t.category) return ''
+        return t.category
+            .toLowerCase()
+            .replace(/_/g, ' ')
+            .replace(/\b\w/g, (c) => c.toUpperCase())
+            .replace(/\b(And|Or)\b/g, (c) => c.toLowerCase())
     }
 }
