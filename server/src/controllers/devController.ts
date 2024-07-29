b6d7f267c96ff3bc87fa7f336357b9e8978bb314
aditya-arcot <arcot.aditya@utexas.edu>
2024-07-29 03:21:31 -0500

    refactor dev routes

---
+++ server/src/controllers/devController.ts
@@ -0,0 +1,110 @@
+import { Request, Response } from 'express'
+import { SandboxItemFireWebhookRequestWebhookCodeEnum } from 'plaid'
+import { HttpError } from '../models/httpError.js'
+import {
+    retrieveItemById,
+    retrieveItemByUserIdAndInstitutionId,
+    retrieveItemsByUserId,
+    updateItemToInactive,
+} from '../models/item.js'
+import { deleteUser, retrieveUsers, User } from '../models/user.js'
+import {
+    createPublicToken,
+    exchangePublicTokenAndCreateItemAndSync,
+    fireWebhook,
+    removeItem,
+    syncItemData,
+} from '../services/plaidService.js'
+import { logger } from '../utils/logger.js'
+
+export const deleteUsers = async (req: Request, res: Response) => {
+    logger.debug('deleting all users')
+
+    const users = await retrieveUsers()
+    await Promise.all(
+        users.map(async (user) => {
+            logger.debug({ user }, 'deleting user')
+            const items = await retrieveItemsByUserId(user.id)
+            await Promise.all(items.map((item) => removeItem(item)))
+            await deleteUser(user.id)
+        })
+    )
+
+    req.session.destroy(() => res.status(204).send())
+}
+
+export const deactivateItems = async (_req: Request, res: Response) => {
+    logger.debug('deactivating all items')
+
+    const users = await retrieveUsers()
+    await Promise.all(
+        users.map(async (user) => {
+            logger.debug({ user }, 'deactivating items for user')
+
+            const items = await retrieveItemsByUserId(user.id)
+            await Promise.all(
+                items.map(async (item) => {
+                    await removeItem(item)
+                    await updateItemToInactive(item.id)
+                })
+            )
+        })
+    )
+
+    return res.status(204).send()
+}
+
+export const fireSandboxWebhook = async (req: Request, res: Response) => {
+    const itemId: string | undefined = req.query['itemId'] as string
+    if (!itemId) throw Error('missing item id')
+
+    const item = await retrieveItemById(itemId)
+    if (!item) throw Error('item not found')
+
+    const code: string | undefined = req.query['code'] as string
+    const codeEnum = code as SandboxItemFireWebhookRequestWebhookCodeEnum
+    if (
+        !codeEnum ||
+        !Object.values(SandboxItemFireWebhookRequestWebhookCodeEnum).includes(
+            codeEnum
+        )
+    ) {
+        throw Error('invalid webhook code')
+    }
+
+    await fireWebhook(item, codeEnum)
+    return res.status(204).send()
+}
+
+export const createSandboxItem = async (req: Request, res: Response) => {
+    const user: User | undefined = req.session.user
+    if (!user) throw new HttpError('missing user', 400)
+
+    const institutionId = 'ins_56'
+    const institutionName = 'Chase'
+    const item = await retrieveItemByUserIdAndInstitutionId(
+        user.id,
+        institutionId
+    )
+    if (item) throw new HttpError('account already exists', 409)
+
+    const publicToken = await createPublicToken(user, institutionId)
+    await exchangePublicTokenAndCreateItemAndSync(
+        user.id,
+        institutionId,
+        institutionName,
+        publicToken
+    )
+    return res.status(204).send()
+}
+
+export const syncItem = async (req: Request, res: Response) => {
+    const itemId: string | undefined = req.query['itemId'] as string
+    if (!itemId) throw new HttpError('missing item id', 400)
+
+    const item = await retrieveItemById(itemId)
+    if (!item) throw new HttpError('item not found', 404)
+
+    await syncItemData(item)
+    return res.status(204).send()
+}
--- server/src/routes/devRoutes.ts
+++ server/src/routes/devRoutes.ts
@@ -1,22 +1,12 @@
-import express, { Request, Response } from 'express'
-import { SandboxItemFireWebhookRequestWebhookCodeEnum } from 'plaid'
-import { HttpError } from '../models/httpError.js'
+import express from 'express'
 import {
-    retrieveItemById,
-    retrieveItemByUserIdAndInstitutionId,
-    retrieveItemsByUserId,
-    updateItemToInactive,
-} from '../models/item.js'
-import { deleteUser, retrieveUsers, User } from '../models/user.js'
-import {
-    createPublicToken,
-    exchangePublicTokenAndCreateItemAndSync,
-    fireWebhook,
-    removeItem,
-    syncItemData,
-} from '../services/plaidService.js'
+    createSandboxItem,
+    deactivateItems,
+    deleteUsers,
+    fireSandboxWebhook,
+    syncItem,
+} from '../controllers/devController.js'
 import { catchAsync } from '../utils/catchAsync.js'
-import { logger } from '../utils/logger.js'
 
 const router = express.Router()
 
@@ -37,23 +27,7 @@ const router = express.Router()
  *       204:
  *         description: All users deleted
  */
-router.route('/users').delete(
-    catchAsync(async (req: Request, res: Response) => {
-        logger.debug('deleting all users')
-
-        const users = await retrieveUsers()
-        await Promise.all(
-            users.map(async (user) => {
-                logger.debug({ user }, 'deleting user')
-                const items = await retrieveItemsByUserId(user.id)
-                await Promise.all(items.map((item) => removeItem(item)))
-                await deleteUser(user.id)
-            })
-        )
-
-        req.session.destroy(() => res.status(204).send())
-    })
-)
+router.route('/users').delete(catchAsync(deleteUsers))
 
 /**
  * @swagger
@@ -65,28 +39,7 @@ router.route('/users').delete(
  *       204:
  *         description: All items deactivated
  */
-router.route('/items').delete(
-    catchAsync(async (_req: Request, res: Response) => {
-        logger.debug('deactivating all items')
-
-        const users = await retrieveUsers()
-        await Promise.all(
-            users.map(async (user) => {
-                logger.debug({ user }, 'deactivating items for user')
-
-                const items = await retrieveItemsByUserId(user.id)
-                await Promise.all(
-                    items.map(async (item) => {
-                        await removeItem(item)
-                        await updateItemToInactive(item.id)
-                    })
-                )
-            })
-        )
-
-        return res.status(204).send()
-    })
-)
+router.route('/items').delete(catchAsync(deactivateItems))
 
 /**
  * @swagger
@@ -111,33 +64,7 @@ router.route('/items').delete(
  *       204:
  *         description: Webhook fired
  */
-router.route('/webhook').post(
-    catchAsync(async (req: Request, res: Response) => {
-        const itemId: string | undefined = req.query['itemId'] as string
-        if (!itemId) throw Error('missing item id')
-
-        const item = await retrieveItemById(itemId)
-        if (!item) throw Error('item not found')
-
-        const code: string | undefined = req.query['code'] as string
-        if (
-            !code ||
-            typeof code !== 'string' ||
-            !Object.values(
-                SandboxItemFireWebhookRequestWebhookCodeEnum
-            ).includes(code as SandboxItemFireWebhookRequestWebhookCodeEnum)
-        ) {
-            throw Error('invalid webhook code')
-        }
-        code
-        await fireWebhook(
-            item,
-            code as SandboxItemFireWebhookRequestWebhookCodeEnum
-        )
-
-        return res.status(204).send()
-    })
-)
+router.route('/webhook').post(catchAsync(fireSandboxWebhook))
 
 /**
  * @swagger
@@ -149,29 +76,7 @@ router.route('/webhook').post(
  *       204:
  *         description: Created and synced item
  */
-router.route('/item').post(
-    catchAsync(async (req: Request, res: Response) => {
-        const user: User | undefined = req.session.user
-        if (!user) throw new HttpError('missing user', 400)
-
-        const institutionId = 'ins_56'
-        const institutionName = 'Chase'
-        const item = await retrieveItemByUserIdAndInstitutionId(
-            user.id,
-            institutionId
-        )
-        if (item) throw new HttpError('account already exists', 409)
-
-        const publicToken = await createPublicToken(user, institutionId)
-        await exchangePublicTokenAndCreateItemAndSync(
-            user.id,
-            institutionId,
-            institutionName,
-            publicToken
-        )
-        return res.status(204).send()
-    })
-)
+router.route('/item').post(catchAsync(createSandboxItem))
 
 /**
  * @swagger
@@ -189,18 +94,7 @@ router.route('/item').post(
  *       204:
  *         description: Synced item
  */
-router.route('/item-sync').post(
-    catchAsync(async (req: Request, res: Response) => {
-        const itemId: string | undefined = req.query['itemId'] as string
-        if (!itemId) throw new HttpError('missing item id', 400)
-
-        const item = await retrieveItemById(itemId)
-        if (!item) throw new HttpError('item not found', 404)
-
-        await syncItemData(item)
-        return res.status(204).send()
-    })
-)
+router.route('/item-sync').post(catchAsync(syncItem))
 
 // TODO - reset item login
 // https://plaid.com/docs/api/sandbox/#sandboxitemreset_login
