6107039f2fb2cb7b639b2bb8e85eeb3f6a9f7540
aditya-arcot <arcot.aditya@utexas.edu>
2024-08-05 18:09:16 -0500

    create deletion script

---
+++ database/scripts/delete_old_records.sql
@@ -0,0 +1,21 @@
+START TRANSACTION;
+
+DELETE FROM audit
+WHERE create_timestamp < NOW() - INTERVAL '1 week';
+
+DELETE FROM app_requests
+WHERE create_timestamp < NOW() - INTERVAL '1 week';
+
+DELETE FROM plaid_link_events
+WHERE create_timestamp < NOW() - INTERVAL '1 week';
+
+DELETE FROM plaid_api_requests
+WHERE create_timestamp < NOW() - INTERVAL '1 week';
+
+DELETE FROM webhooks
+WHERE create_timestamp < NOW() - INTERVAL '1 week';
+
+DELETE FROM jobs
+WHERE create_timestamp < NOW() - INTERVAL '1 week';
+
+COMMIT;
