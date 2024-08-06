682d28125a92d116d34f9616a2e9efaf1c996189
aditya-arcot <arcot.aditya@utexas.edu>
2024-08-06 12:42:51 -0500

    format account type, subtype

--- server/src/plaid/accountMethods.ts
+++ server/src/plaid/accountMethods.ts
@@ -1,5 +1,6 @@
 import { AccountBase } from 'plaid'
 import { Account } from '../models/account.js'
+import { toTitleCase } from '../utils/format.js'
 
 export const mapPlaidAccount = (
     account: AccountBase,
@@ -17,7 +18,53 @@ export const mapPlaidAccount = (
         isoCurrencyCode: account.balances.iso_currency_code,
         unofficialCurrencyCode: account.balances.unofficial_currency_code,
         creditLimit: account.balances.limit,
-        type: account.type,
-        subtype: account.subtype,
+        type: toTitleCase(account.type),
+        subtype: mapAccountSubtype(account.subtype),
     }
 }
+
+const mapAccountSubtype = (subtype: string | null): string | null => {
+    if (!subtype) return null
+
+    if (plaidLowercaseSubtypes.includes(subtype)) return subtype.toLowerCase()
+    if (plaidUppercaseSubtypes.includes(subtype)) return subtype.toUpperCase()
+
+    if (subtype === 'paypal') return 'PayPal'
+    if (subtype === 'line of credit') return 'Line of Credit'
+    if (subtype === 'cash isa') return 'Cash ISA'
+    if (subtype === 'non-custodial wallet') return 'Non-Custodial Wallet'
+    if (subtype === 'non-taxable brokerage account')
+        return 'Non-Taxable Brokerage Account'
+
+    return toTitleCase(subtype)
+}
+
+const plaidLowercaseSubtypes = ['403B']
+
+const plaidUppercaseSubtypes = [
+    'hsa',
+    'cd',
+    'ebt',
+    'gic',
+    'hsa',
+    'ira',
+    'isa',
+    'lif',
+    'lira',
+    'lrif',
+    'lrsp',
+    'prif',
+    'qshr',
+    'rdsp',
+    'resp',
+    'rlif',
+    'rrif',
+    'rrsp',
+    'sarsep',
+    'sep ira',
+    'simple ira',
+    'sipp',
+    'tfsa',
+    'ugma',
+    'utma',
+]
