fba106f01134bc9f18076056ee8ddb735dacc9a0
aditya-arcot <arcot.aditya@utexas.edu>
2024-08-19 00:11:34 -0500

    add empty spending, net worth, paychecks pages

---
+++ client/src/app/pages/net-worth/net-worth.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-net-worth',
+    standalone: true,
+    imports: [],
+    templateUrl: './net-worth.component.html',
+})
+export class NetWorthComponent {}
---
+++ client/src/app/pages/paychecks/paychecks.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-paychecks',
+    standalone: true,
+    imports: [],
+    templateUrl: './paychecks.component.html',
+})
+export class PaychecksComponent {}
