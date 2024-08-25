9c6a184ee6199fa035a27a2966ff5a6bb432ac30
aditya-arcot <arcot.aditya@utexas.edu>
2024-08-25 16:51:24 -0500

    preserve plaid api requests table column order

--- database/scripts/migrations/2024_08_25-01_add_error_cols_to_plaid_requests_table.sql
+++ database/scripts/migrations/2024_08_25-01_add_error_cols_to_plaid_requests_table.sql
@@ -2,6 +2,62 @@
 add error code, error response columns to plaid api requests table
 */
 
+BEGIN;
+
+CREATE TABLE plaid_api_requests_migrate (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER,
+    item_id INTEGER,
+    timestamp TIMESTAMPTZ NOT NULL,
+    duration INTEGER NOT NULL,
+    method TEXT NOT NULL,
+    params JSON NOT NULL,
+    response JSON,
+    error_code INTEGER,
+    error_name TEXT,
+    error_message TEXT,
+    error_response TEXT,
+    error_stack TEXT,
+    create_timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO plaid_api_requests_migrate (
+    user_id, 
+    item_id, 
+    timestamp, 
+    duration, 
+    method, 
+    params, 
+    response, 
+    error_name, 
+    error_message, 
+    error_stack, 
+    create_timestamp
+)
+SELECT
+    user_id,
+    item_id,
+    timestamp,
+    duration,
+    method,
+    params,
+    response,
+    error_name,
+    error_message,
+    error_stack,
+    create_timestamp
+FROM
+    plaid_api_requests;
+
+DROP TABLE plaid_api_requests;
+
+ALTER TABLE plaid_api_requests_migrate
+RENAME TO plaid_api_requests;
+
+ALTER SEQUENCE plaid_api_requests_migrate_id_seq
+RENAME TO plaid_api_requests_id_seq;
+
 ALTER TABLE plaid_api_requests
-ADD COLUMN error_code INTEGER,
-ADD COLUMN error_response TEXT;
+RENAME CONSTRAINT plaid_api_requests_migrate_pkey TO plaid_api_requests_pkey;
+
+COMMIT;
