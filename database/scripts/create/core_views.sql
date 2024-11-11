4dd9acdc1f0db3facfd586f749e61d57806c9d9c
aditya-arcot <arcot.aditya@utexas.edu>
2024-11-11 16:27:01 -0600

    create liabilities views

--- database/scripts/create/core_views.sql
+++ database/scripts/create/core_views.sql
@@ -47,6 +47,27 @@ JOIN securities s
 	ON h.security_id = s.id
 ORDER BY h.account_id, h.id;
 
+CREATE VIEW core.active_credit_card_liabilities AS
+SELECT a.user_id, c.*
+FROM credit_card_liabilities c
+JOIN active_accounts a
+    ON c.account_id = a.id
+ORDER BY c.id;
+
+CREATE VIEW core.active_mortgage_liabilities AS
+SELECT a.user_id, m.*
+FROM mortgage_liabilities m
+JOIN active_accounts a
+    ON m.account_id = a.id
+ORDER BY m.id;
+
+CREATE VIEW core.active_student_loan_liabilities AS
+SELECT a.user_id, s.*
+FROM student_loan_liabilities s
+JOIN active_accounts a
+    ON s.account_id = a.id
+ORDER BY s.id;
+
 CREATE VIEW core.active_notifications AS
 SELECT *
 FROM notifications
