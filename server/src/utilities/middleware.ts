9aa6c1d361b087080fec7db8a170fb23c2e07c03
aditya-arcot <arcot.aditya@utexas.edu>
2025-05-19 21:10:49 -0500

    remove default csrf cookie option

--- server/src/utilities/middleware.ts
+++ server/src/utilities/middleware.ts
@@ -55,7 +55,6 @@ export const createCsrfMiddleware = () => {
         cookieOptions: {
             secure: production,
             maxAge: 1000 * 60 * 60 * 24, // 1 day,
-            sameSite: 'strict' as const,
         },
     }
     const { doubleCsrfProtection } = doubleCsrf(options)
